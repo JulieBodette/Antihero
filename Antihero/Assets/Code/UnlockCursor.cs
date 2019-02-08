eb90c36770f086300ed38cf7ed365028ad106a88
Julie Desktop\epicpython <jr3python@gmail.com>
2019-02-08 15:29:11 -0500

    made unity project with player
    Made a unity project with player scripts and some basic scripts for lading scenes.

---
+++ Antihero/Assets/Code/UnlockCursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockCursor : MonoBehaviour
+{
+    //use to unlock cursor in gameover and win screens
+    //previosly the cursor was locked for better player movement in scene Poop
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Debug.Log("Unlocking Cursor");
+    }
+
+}

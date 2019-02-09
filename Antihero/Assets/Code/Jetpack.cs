c8b8c15c2970e90294deb21b6ea2a3dcbe9352a7
Julie Desktop\epicpython <jr3python@gmail.com>
2019-02-09 14:55:06 -0500

    added jetpack

---
+++ Antihero/Assets/Code/Jetpack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetpack : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("got jetpack");
+            RetainData.data.HasJetpack = true; //set variable to true 
+            //player class checks this variable to see if it can fly
+            Destroy(this.gameObject); //destroy the jetpack once it has been picked up
+        }
+       
+    }
+}
---
+++ Antihero/Assets/Code/RetainData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetainData : MonoBehaviour
+{
+    public static RetainData data;//nice static reference
+    // Start is called before the first frame update
+    public bool HasJetpack;//set to true if player has jetpack
+    void Awake()
+    {
+        if (data == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            data = this;
+        }
+        else if (data != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

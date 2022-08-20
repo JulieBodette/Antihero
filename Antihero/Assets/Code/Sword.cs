0d512d2763800e0130d7bf6869651995923105d1
JulieBodette <jr3python@gmail.com>
2022-08-19 23:02:29 -0400

    Added Quit on Esc and sword

---
+++ Antihero/Assets/Code/Sword.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "BreakableBarrier")
+        {
+            Debug.Log("collide with barrier");
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+
+}
